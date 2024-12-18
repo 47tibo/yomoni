8c7a449a94eb8c153ef458603b3ee58dd75d5ef0
Thibaut Gilbert <47tibo@gmail.com>
2024-12-18 09:48:33 +0100

    feat(app): add base layout for home page
    page is responsive & contains yomoni logo on large screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
+import { Image } from "@/components/ui/image";
+import { SafeAreaView } from "@/components/ui/safe-area-view";
+import { ScrollView } from "@/components/ui/scroll-view";
+import { VStack } from "@/components/ui/vstack";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="w-full h-full">
+      <ScrollView
+        className="w-full h-full"
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <HStack className="w-full bg-background-0 flex-grow justify-center">
+          <VStack className="md:items-center md:justify-center flex-1 w-full p-9 md:gap-10 gap-16 md:m-auto md:w-1/2 h-full">
+            <Heading size="2xl">champ de recherche ici</Heading>
+          </VStack>
+
+          <VStack
+            className="relative hidden md:flex h-full w-full flex-1 items-center justify-center"
+            space="md"
+          >
+            <Image
+              height={100}
+              width={100}
+              size="2xl"
+              source={require("@/assets/images/logo.jpeg")}
+              className="object-cover"
+              alt="yomoni logo"
+            />
+          </VStack>
+        </HStack>
+        <VStack className="w-full bg-background-0 p-4">
+          <Heading size="2xl">résultats affichés ici</Heading>
+        </VStack>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
